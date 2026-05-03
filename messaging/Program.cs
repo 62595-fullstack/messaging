95826e81814370762e4d8556b9e3bfe6673fc774
Toby <60399231+TobyNatooor@users.noreply.github.com>
2026-05-03 16:26:30 +0200

    removed uneeded swagger and authorization

--- messaging/Program.cs
+++ messaging/Program.cs
@@ -1,23 +1,12 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore.Infrastructure;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using System.Reflection;
-using System.Text;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Services.MessageService;
 using Microsoft.Extensions.Hosting;
-
-JsonConvert.DefaultSettings = () => new JsonSerializerSettings
-{
-	ContractResolver = new CamelCasePropertyNamesContractResolver()
-};
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
@@ -37,65 +26,9 @@ string host = config["host"] ?? "";
 builder.WebHost.UseUrls($"https://{host}:{programPort}");
 
 builder.Services.AddGrpc();
-// builder.Services.AddGrpcClient<MessageService>(options =>
-// {
-// 	options.Address = new Uri($"http://{host}:{programPort}");
-// });
-
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen(options =>
-// {
-// 	options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
-// 	{
-// 		Type = SecuritySchemeType.Http,
-// 		Scheme = "bearer",
-// 		BearerFormat = "JWT",
-// 		Description = "JWT Authorization header using the Bearer scheme."
-// 	});
-// 	options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
-// 	{
-// 		[new OpenApiSecuritySchemeReference("bearer", document)] = []
-// 	});
-// });
-// builder.Services.ConfigureHttpJsonOptions(options =>
-// {
-// 	options.SerializerOptions.PropertyNameCaseInsensitive = true;
-// });
-// builder.Services.AddCors(options =>
-// {
-// 	options.AddDefaultPolicy(policy =>
-// 	{
-// 		policy.WithOrigins($"http://{host}:3000")
-// 			  .AllowAnyHeader()
-// 			  .AllowAnyMethod();
-// 	});
-// });
-
-// builder.Services
-// 	.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-// 	.AddJwtBearer(options =>
-// 			{
-// 				options.RequireHttpsMetadata = false;
-// 				options.TokenValidationParameters = new TokenValidationParameters
-// 				{
-// 					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Secret"]!)),
-// 					ValidIssuer = $"http://{host}:{programPort}",
-// 					ValidAudience = config["Jwt:Audience"],
-// 					ClockSkew = TimeSpan.Zero,
-// 					ValidIssuers = [
-// 					$"http://{host}:{programPort}"
-// 					],
-// 				};
-// 			});
-// builder.Services.AddAuthorization();
 
 WebApplication app = builder.Build();
 app.MapGrpcService<MessageService>();
-
-// app.UseAuthentication();
-// app.UseAuthorization();
-// app.UseStaticFiles();
-// app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -104,14 +37,6 @@ if (app.Environment.IsDevelopment())
 	{
 		await db.GetService<IMigrator>().MigrateAsync();
 	}
-
-	// app.UseSwagger();
-	// // SwaggerUI can be viewed at http://localhost:{port}
-	// app.UseSwaggerUI(options =>
-	// {
-	// 	options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-	// 	options.RoutePrefix = string.Empty;
-	// });
 }
 else
 {
