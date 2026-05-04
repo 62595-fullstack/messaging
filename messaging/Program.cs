6cce44f3d205cc2e591a8f71abc9ad14361516da
Toby <60399231+TobyNatooor@users.noreply.github.com>
2026-05-04 09:43:12 +0200

    added jwt back

--- messaging/Program.cs
+++ messaging/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Services.MessageService;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(new WebApplicationOptions
 {
@@ -26,8 +29,26 @@ string host = config["host"] ?? "";
 builder.WebHost.UseUrls($"https://{host}:{programPort}");
 
 builder.Services.AddGrpc();
-
+builder.Services
+	.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+			{
+				options.RequireHttpsMetadata = false;
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Secret"]!)),
+					ValidIssuer = $"http://{host}:{programPort}",
+					ValidAudience = config["Jwt:Audience"],
+					ClockSkew = TimeSpan.Zero,
+					ValidIssuers = [
+					$"http://{host}:{programPort}"
+					],
+				};
+			});
+builder.Services.AddAuthorization();
 WebApplication app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapGrpcService<MessageService>();
 
 // Configure the HTTP request pipeline.
